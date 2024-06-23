d066b7d337dfe3d40f94c4f9df6fd3c6c9c0d1e1
Matteo <matt.dag09@gmail.com>
2024-06-23 12:56:32 -0400

    fixed cancel button

--- project2/src/Pages/AddRecipe.js
+++ project2/src/Pages/AddRecipe.js
@@ -197,7 +197,7 @@ function AddRecipe() {
           <div className="flex justify-end gap-4 mt-8">
           <button
             type="submit"
-            class="text-white border-2 border-blue-600  text-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-md w-full sm:w-auto px-5 py-2.5 text-center"
+            class="font-semibold border-2 border-blue-600  text-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-3xl text-md w-full sm:w-auto px-5 py-2.5 text-center"
           >
             Cancel
           </button>
