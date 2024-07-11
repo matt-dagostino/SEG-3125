cf4f5622981520a661dbbec07a427b19227c9809
Matteo <matt.dag09@gmail.com>
2024-07-11 00:43:05 -0400

    SR accessibility add

--- project2-step5/src/Pages/Zoom.js
+++ project2-step5/src/Pages/Zoom.js
@@ -4,31 +4,38 @@ import Footer from "../components/Footer";
 
 function Zoom() {
   return (
-    <div>
+    <div role="main">
       <Navbar location="zoom" />
+
       <div className="flex flex-col justify-center items-center pt-8 gap-4 pb-12">
         <h1 className="text-5xl font-extrabold uppercase text-heading-color text-center">
-          live interactive cooking sessions with us
+          Live Interactive Cooking Sessions with Us
         </h1>
         <p className="font-semibold text-heading-color">
           Join us live every Mondays, Wednesdays and Fridays at 6pm EST to cook
           together!
         </p>
 
-        <div className="flex justify-center items-center bg-gray-200 rounded-lg overflow-hidden shadow-md w-1/2 mt-8">
+        <div
+          className="flex justify-center items-center bg-gray-200 rounded-lg overflow-hidden shadow-md w-1/2 mt-8"
+          role="region"
+          aria-label="Live Cooking Sessions"
+        >
           <iframe
             className="w-full h-96"
             src="https://www.youtube.com/embed/O5l1SMavklI"
-            title="YouTube video player"
-            alt="Live Cooking Sessions"
+            title="Live Cooking Sessions"
+            aria-label="Live Cooking Sessions Video Player"
             allowFullScreen
           ></iframe>
         </div>
+
         <p className="font-semibold text-heading-color text-sm pb-4">
           Since we aren't live currently, feel free to watch our previous
           sessions above!
         </p>
       </div>
+
       <Footer />
     </div>
   );
